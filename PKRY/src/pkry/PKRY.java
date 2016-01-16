feb8b8e882458ae9e58ed82d671c08d843e1a39b
rrzatkie <rafal.rzatkiewicz@gmail.com>
2016-01-16 16:43:04 +0100

    drugi commit

--- PKRY/src/pkry/PKRY.java
+++ PKRY/src/pkry/PKRY.java
@@ -16,6 +16,13 @@ public class PKRY {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        
     }
     
 }
