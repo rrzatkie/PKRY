cf60450b4b0aba890cb283d0c2e1edf701069ea7
skmonia <skowron.monika@yahoo.com>
2016-01-22 13:47:49 +0100

    sign cd

--- PKRY_M/src/pkry_m/SIGN.java
+++ PKRY_M/src/pkry_m/SIGN.java
@@ -6,6 +6,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -75,16 +77,44 @@ public class SIGN {
         h = new BigInteger(gpsk[4] , 2);
         b = new BigInteger(gpsk[5] , 2);
        // bsn=new BigInteger(u[2], 16);
-       
+       //TODO wczytać plik z kluczem prywatnym uczestnika grupy jako e, x , Ag
        /**wartosci wczytane to teraz generujemy nasze liczby*/
        //f=(H(bsn))^2 mod(n)
+       MessageDigest mda = null;    
+        try {
+            mda = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(JOIN.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        byte[] coded = mda.digest(bsn.toString().getBytes());
+        //
+        BigInteger hsh=new BigInteger(1,coded);
+  //TODO skrócić hsh do 4 B
+       f=hsh.modPow(TWO, n) ;
        w1=genRandom(1,2*lp, 0,0,0);
        w2=genRandom(1,2*lp, 0,0,0);
        w3=genRandom(1,2*lp, 0,0,0);
        T1=Ag.multiply(b.modPow(w1,n));
-       T2=(g_.modPow(w1,BigInteger.ONE).multiply(h.modPow(w2,BigInteger.ONE))).mod(n);
-       T3=(g_.modPow(e,BigInteger.ONE).multiply(h.modPow(w3,BigInteger.ONE))).mod(n);
+      
+       // (g_.modPow(s1, n)
+       T2=(g_.modPow(w1,n).multiply(h.modPow(w2,n))).mod(n);
+       T3=(g_.modPow(e,n).multiply(h.modPow(w3,n))).mod(n);
        T4=f.modPow(x, n);
+       /**gererujemy r-y ktore przydadza sie przy liczeniu parametrów d*/
+       BigInteger r1=genRandom(eps,0,k,0,le );
+       BigInteger r2=genRandom(eps,0,k,lx,0 );
+       BigInteger r3=genRandom(eps,2*lp,k,0,0 );
+       BigInteger r4=genRandom(eps,2*lp,k,0,0 );
+       BigInteger r5=genRandom(eps,2*lp,k,0,0 );
+       BigInteger r9=genRandom(eps,2*lp,k,0,le );
+       BigInteger r10=genRandom(eps,2*lp,k,0,le );
+       /**obliczamy d1-d5*/
+       BigInteger d5=f.modPow(r2, n);
+       BigInteger d4= (g_.modPow(r1,n).multiply(h.modPow(r5, n))).mod(n);
+       BigInteger d3= (g_.modPow(r3,n).multiply(h.modPow(r4, n)).mod(n));
+       //TODO d2
+       //TODO d1
+       /**c=*/
     }
     /**metoda do generowania randomowej liczby z przedziału [0, 2^(eps*(2lp+k+le +lx))-1]*/
     private static BigInteger genRandom(int eps, int lp, int k, int lx, int le)
